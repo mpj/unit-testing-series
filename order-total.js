f102d6a8dd625ab19e003a4d23788ab0851a024c
Mattias Petter Johansson <mpj@mpj.me>
2017-12-08 14:50:29 +0100

    removed some duplication

--- order-total.js
+++ order-total.js
@@ -1,4 +1,7 @@
 function orderTotal(fetch, process, order) {
+  const sumOrderItems = order =>
+    order.items.reduce((prev, cur) =>
+      cur.price * (cur.quantity || 1) + prev, 0)
   if(order.country) {
     return fetch('https://vatapi.com/v1/country-code-check?code=' + order.country, {
       headers: {
@@ -7,11 +10,9 @@ function orderTotal(fetch, process, order) {
     })
       .then(response => response.json())
       .then(data => data.rates.standard.value)
-      .then(vat => order.items.reduce((prev, cur) =>
-        cur.price * (cur.quantity || 1) + prev, 0) * (1+vat/100))
+      .then(vat => sumOrderItems(order) * (1+vat/100))
   }
-  return Promise.resolve(order.items.reduce((prev, cur) =>
-    cur.price * (cur.quantity || 1) + prev, 0))
+  return Promise.resolve(sumOrderItems(order))
 }
 
 module.exports = orderTotal
